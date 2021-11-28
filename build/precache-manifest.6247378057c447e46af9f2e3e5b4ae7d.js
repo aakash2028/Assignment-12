6a465e9df997527ef6d3b7b78f17f70cf6ad8a91
aakash2028 <ajoshi3054@sdsu.edu>
2021-11-28 12:23:46 -0800

    initial commit

---
+++ build/precache-manifest.6247378057c447e46af9f2e3e5b4ae7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5be2a939f28a08054020486856a5aefc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb1e3e093768f1c1d493",
+    "url": "/static/css/main.8a815b0d.chunk.css"
+  },
+  {
+    "revision": "4b06bd295ca8b5dc54be",
+    "url": "/static/js/2.7e1262a9.chunk.js"
+  },
+  {
+    "revision": "fb1e3e093768f1c1d493",
+    "url": "/static/js/main.fabda054.chunk.js"
+  },
+  {
+    "revision": "d83ead9cef5f9073a4d2",
+    "url": "/static/js/runtime-main.0f5120b6.js"
+  }
+]);
